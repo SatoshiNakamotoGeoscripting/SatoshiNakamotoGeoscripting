5895ab6af2770111850d5280ab0efe25b5cdcc72
hectormauer <muro.hector@gmail.com>
2017-01-14 14:27:48 +0100

    Added new function for preproces and fixed relative paths

--- Lecture5/main.R
+++ Lecture5/main.R
@@ -3,8 +3,8 @@
 # 01/13/2017
 
 #Set and check your working directory if necessary
-#setwd()
-#getwd()
+setwd("/home/user/mytest/Lecture5")
+getwd()
 
 #Import libraries
 library(raster)
@@ -14,28 +14,14 @@ source('R/cleaningClouds.R')
 source('R/ndvi.R')
 
 # 1st: Download the files from the browser & Unpack them
-download.file(url = 'https://www.dropbox.com/s/akb9oyye3ee92h3/LT51980241990098-SC20150107121947.tar.gz?dl=0', 
-              destfile = 'landsat5.tar.gz', method = 'wget')
-download.file(url = 'https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=0#', 
-              destfile = 'landsat8.tar.gz', method = 'wget')
 
-#Unpack (untar) them and create a folder per each image
-untar('landsat5.tar.gz',exdir='LT5')
-untar('landsat8.tar.gz',exdir='LT8')
-#exdir will create two folders containing the two Landsat images, in the working directory.
+source('R/preproces.R')
 
-# 2nd: Create Stacks from the multiple layers that have been unpacked
-#Create two lists with the file names in order to create a stack/brick afterwards.
-listLT5 <- list.files('./LT5' ,pattern = glob2rx('*.tif'), full.names = TRUE)
-listLT8 <- list.files('./LT8' ,pattern = glob2rx('*.tif'), full.names = TRUE)
-
-LT5stack <- stack(listLT5)
-LT8stack <- stack(listLT8)
- 
+# Plotting clouds mask for both images. 
 plot(LT5stack[[1]], main="Clouds mask in Landsat TM 5 image, 1990")
 plot(LT8stack[[1]], main="Clouds mask in Landsat OLI image, 2014")
 
-# 3rd: Cleaning the clouds. We use the first layer from the rasters, which contains the clouds mask that will be used 
+# 2nd: Cleaning the clouds. We use the first layer from the rasters, which contains the clouds mask that will be used 
 #to clear out the clous from all the layers.
 #We call the function that uses an algorithm found in the tutorial. 
 #The inputs are the raster and its position in the whole range of layers.
@@ -51,7 +37,7 @@ LT8_nocloud<-cleaningClouds(LT8stack,1)
 plot(LT5_nocloud[[2]])
 plot(LT8_nocloud[[2]])
 
-# 4th: Applying NDVI
+# 3rd: Applying NDVI
 #We call the function NDVI which has as inputs the red and NIR bands for every image.
 #We use overlay in order to keep more RAM free.
 
@@ -60,13 +46,14 @@ plot(LT5_ndvi, main="NDVI for Landsat TM 5, 1990")
 LT8_ndvi <- overlay(LT8_nocloud[[4]],LT8_nocloud[[5]],fun=ndviCalc)
 plot(LT8_ndvi, main="NDVI for Landsat 8 OLI, 2014")
 
-# 5th: Intersect both imagesto keep with the piece they share so they can be substracted (croping)
+# 4th: Intersect both imagesto keep with the piece they share so they can be substracted (croping)
 LT5_crop <- intersect(LT5_ndvi,LT8_ndvi)
 LT8_crop <- intersect(LT8_ndvi,LT5_ndvi)
 
-# 6th: Substraction. By substracting we will be able to see those parts of the image where the NDVI has increased or decreased.
+# 5th: Substraction. By substracting we will be able to see those parts of the image where the NDVI has increased or decreased.
 NDVI_dif = LT8_crop - LT5_crop
 plot(NDVI_dif, main="Difference between 2014's NDVI and 1990's NDVI")
 
 #We create a raster from the last calculation
-writeRaster(x=NDVI_dif, filename="NDVI_differences.tif", datatype='FLT4S',overwrite= TRUE)
+dir.create('./output')
+writeRaster(x=NDVI_dif, filename="output/NDVI_differences.tif", datatype='FLT4S',overwrite= TRUE)
