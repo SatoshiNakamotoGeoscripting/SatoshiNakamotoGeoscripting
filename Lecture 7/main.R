2a6a3f53b10ce9409d167fcfad8d2ece01440086
Bixbeat <bixbeat@github.com>
2017-01-17 17:39:21 +0100

    Added all required examples for lecture 7

--- Lecture 7/main.R
+++ Lecture 7/main.R
@@ -9,6 +9,14 @@ preprocessData('https://raw.githubusercontent.com/GeoScripting-WUR/VectorRaster/
 
 greenValues <- computeGreenness("MOD", "NLD", 2)
 
+#Plot for one month
+greenestUnit <- getGreenestInTimespan(greenValues, c("May"))
+displayAdministrativeUnit(greenestUnit, 2)
+
 #For several months at once - add all months in a year to check the yearly average
 greenestUnit <- getGreenestInTimespan(greenValues, c("May","June","July"))
 displayAdministrativeUnit(greenestUnit, 2)
+
+#For all months in the year
+greenestUnit <- getGreenestInTimespan(greenValues, c("January","February","March","April","May","June","July","August","September","October","November","December"))
+
