2a8b5c6d61464f74f0571db533c5d4619b88c346
user <bixbeat@github.com>
2017-01-23 15:19:12 +0100

    Added directory creation & made commenting more verbose

--- Lecture 11/Lecture11.py
+++ Lecture 11/Lecture11.py
@@ -5,13 +5,21 @@ Created on Mon Jan 23 09:08:21 2099
 @author: Hector Muro, Alex Levering
 Team: Satoshi Nakamoto
 """
-## Loading the modules
-from osgeo import ogr,osr
-import os
-import folium
+## Standard libraries
+from os import makedirs, chdir
+from os.path import exists
 from numpy import mean
-import simplekml
 
+## Non-standard libraries
+from osgeo import ogr,osr
+import simplekml
+import folium
+
+#Create data folder if it does not exist
+if not exists('./data'):
+    makedirs('./data')
+
+# Move over to data directory
 os.chdir('./data')
 
 ## Is the ESRI Shapefile driver available?
@@ -22,9 +30,9 @@ if drv is None:
 else:
     print  "%s driver IS available.\n" % driverName
 
-#Set SRS
-
+#Exports list of tuples/lists with WGS coordinates and gives a shp and optionally a kml as output
 def shpFromPoints(filename, layername, points, save_kml = True):
+    #Initialize all the variables needed to set up a SHP
     spatialReference = osr.SpatialReference()
     spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
     ds = drv.CreateDataSource(filename)
@@ -35,25 +43,19 @@ def shpFromPoints(filename, layername, points, save_kml = True):
     feature = ogr.Feature(layerDefinition)
     
     kml = simplekml.Kml()
-    for i, value in enumerate(points):
+    for i, value in enumerate(points): #For each coordinate, add to shp and to the kml if save kml is true
         point.SetPoint(0,value[0], value[1])
         feature.SetGeometry(point)
         layer.CreateFeature(feature)
-        kml.newpoint(name=str(i), coords = [(value[0],value[1])])
+        if save_kml == True:
+            kml.newpoint(name=str(i), coords = [(value[0],value[1])])
     ds.Destroy()        
     if save_kml == True:
         kml.save("my_points.kml")
-    
-#ogr.main(["","-f", "KML", "shit.kml", filename])
-    #ogr2ogr shp to kml
-    
-filename = "wageningenpoints.shp"
-layername = "wagpoints"
-pts = [(51.987398, 5.665777),
-               (51.978434, 5.663133)]
-shpFromPoints(filename, layername, pts)
 
+#Creates a folium HTML with lat/long as iframe popups
 def mapFromPoints(pts, outname, zoom_level, save = True):
+    #Get the center of the map by deriving the mean of all lats/longs
     mean_long = mean([pt[0] for pt in pts])
     mean_lat = mean([pt[1] for pt in pts])
     point_map = folium.Map(location=[mean_long, mean_lat], zoom_start = zoom_level)
@@ -70,4 +72,11 @@ def mapFromPoints(pts, outname, zoom_level, save = True):
         point_map.save("{}.html".format(outname))
     return point_map
 
-mapFromPoints(pts, "DisMyShit", zoom_level = 13)
+filename = "wageningenpoints.shp"
+layername = "wagpoints"
+pts = [ (51.987398, 5.665777),
+        (51.978434, 5.663133),
+        (51.988434, 5.663148) ]
+               
+shpFromPoints(filename, layername, pts)
+mapFromPoints(pts, "satoshinakamoto", zoom_level = 13)
