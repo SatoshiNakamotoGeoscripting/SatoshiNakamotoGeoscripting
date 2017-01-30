d2a26f931a77bb42e6ad7d0476f3b09b02b5a79f
hectormauer <muro.hector@gmail.com>
2017-01-30 12:09:33 +0100

    vader trial uploaded

--- Tweets project/tweetdb.sql
+++ Tweets project/tweetdb.sql
@@ -2,14 +2,16 @@ DROP TABLE trumptweets;
 CREATE TABLE trumptweets (
     id  	bigint,
     time	varchar(500),
-    latitude	varchar(500),
-    longitude	varchar(500),
+    latitude	decimal,
+    longitude	decimal,
     selfrepcity varchar(500),    
     lang	varchar(500),
     source	varchar(500),
     countrycode	varchar(500),
     countryname	varchar(500),
     location	varchar(500),
-    retweet	varchar(500),
-    text        varchar(500)
+    hyperling	varchar(500),
+    text        varchar(500),
+    outlat 	decimal,
+    outlon	decimal
 );
--- Tweets project/vaderTest.py
+++ Tweets project/vaderTest.py
@@ -5,52 +5,77 @@ Created on Fri Jan 27 16:23:07 2017
 
 @author: user
 """
-#https://pypi.python.org/pypi/requests/
-#http://text-processing.com/docs/sentiment.html
-import requests
 ## FOUND HERE http://www.nltk.org/howto/sentiment.html
+## Source code http://www.nltk.org/_modules/nltk/sentiment/vader.html
+## http://www.nltk.org/api/nltk.sentiment.html
+## Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model 
+##for Sentiment Analysis of Social Media Text. Eighth International Conference on
+## Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import psycopg2
 import sys
 reload(sys) #Prevents errors with utf-8 encoding not working properly
 sys.setdefaultencoding('utf8')
+def getTweetsFromDB(dbname, user, password, table_name):
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect("dbname={} user={} password={}".format(dbname, user, password))
+    cursor = conn.cursor()
+    
+    # Retrieve data from database and extract coordinates
+    cursor.execute("SELECT * FROM {} WHERE lang = 'en'".format(table_name))
+    records = cursor.fetchall()
+    
+    list_of_tweets = []
+    for i, record in enumerate(records):
+        tweet_text = record[11].encode('utf-8')
+        tweet_id = record[0]
+        
+        list_of_tweets.append([tweet_id,tweet_text])
+    return list_of_tweets
 
+tweets = getTweetsFromDB("tweets","user","user","trumptweets2")
 
-pos_tweets = []
-neg_tweets = []
-with open('SentimentDataset.csv') as trainingset:
-    i = 0
-    lines = trainingset.readlines()
-    line = lines[0]
-    while i < int((0.005 * len(lines))):
-        string_without_chars = line[:-5]
-        split_line = string_without_chars.split(',')
-        tweet_text = split_line[3:]
-        print tweet_text
-        if string_without_chars[1] == '1':
-            pos_tweets.append((tweet_text, 'positive'))
+def SentimentAnalyzer(tweets):
+    sid = SentimentIntensityAnalyzer() #need to nltk.download() to use all the packages
+    #for tweet in tweets:
+    sentiment_tweets = []
+    for i in range(10): 
+        tweet_id = tweets[i][0]
+        tweet_id = str(tweet_id)
+        tweet_id = int(tweet_id)
+        ss = sid.polarity_scores(tweets[i][1])
+        if ss['compound'] <= -0.293:
+            label = 'negative'
+        elif ss['compound'] >= 0.293:
+            label = 'positive'
         else:
-            neg_tweets.append((tweet_text, 'negative'))
-        i += 1
-        line = lines[i+1]        
+            label = 'neutral'
+        sentiment = ss['compound']
+        
+        sentiment_tweets.append((tweet_id,sentiment,label))
+    return sentiment_tweets
+                  
+sentiment_tweets = SentimentAnalyzer(tweets)
 
-##Create a list of tweets with the tweet and its value              
-labeled_tweets = []
+def insertSentiments(tablename,sentiment_tweets):
 
-for(words,sentiment) in pos_tweets + neg_tweets:
-    #wordsFiltered = [e.lower() for e in words.split() if len(e) >=3] #We discard words with less than two letters
-    labeled_tweets.append((words))#,sentiment))
+    conn = psycopg2.connect("dbname=tweets user=user password=user")
+    cursor = conn.cursor()                   
+    
+    insert = """INSERT INTO {tablename}(sentiment,label) VALUES {sentiment},{label} 
+    """.format(tablename = tablename, 
+                            sentiment = sentiment_tweets[1], 
+                            label = sentiment_tweets[2]) 
+                            #id = sentiment_tweets[0])
+    cursor.execute(insert)
+    #conn.commit()
 
-" ".join(tweet for tweet in labeled_tweets)
-testing = []
-for tweet in labeled_tweets:
-    for word in tweet:
-        testing.append("".join(word))
+insertSentiments("trumptweets2",sentiment_tweets)
 
-
-sid = SentimentIntensityAnalyzer() #need to nltk.download() to use all the packages
-for tweet in testing:
-    print tweet    
-    ss = sid.polarity_scores(tweet)
-    for k in sorted (ss):
-        #print ('%s: %s,' % (k, ss[k]))
-        print('{0}: {1}, '.format(k, ss[k]))
+#    add_columns = """
+#        ALTER TABLE trumptweets2 ADD COLUMN sentiment  decimal;
+#        ALTER TABLE trumptweets2 ADD COLUMN label varchar(15);
+#    """
+#    cursor.execute(add_columns)
+#    conn.commit()
