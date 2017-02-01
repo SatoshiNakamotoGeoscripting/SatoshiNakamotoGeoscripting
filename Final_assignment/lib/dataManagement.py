ada5ef542dfe5e225880ced9d4a5c054c97ccd87
user <ahlevering@hotmail.com>
2017-02-01 16:01:27 +0100

    formatted final assignment

---
+++ Final_assignment/lib/dataManagement.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 11:35:10 2017
+
+@author: user
+"""
+import psycopg2
+from os import system
+
+def getTweetsFromDB(dbname, username, password, sql):
+    """From an existing Tweets database, collect all records"""
+    con = psycopg2.connect("dbname={} user={} password={}".format(dbname, username, password))
+    cur = con.cursor()
+    
+    # Retrieve data from database and extract coordinates
+    cur.execute(sql)
+    records = cur.fetchall()
+    
+    con.close()
+    cur.close()
+    return records
+
+def importPolyJSON(dbname, username, password, geojson, output_table_name):
+    """Import specified geojson file into postgresql spatial db"""
+    # http://gis.stackexchange.com/questions/172092/import-geojson-into-postgis
+    # http://morphocode.com/using-ogr2ogr-convert-data-formats-geojson-postgis-esri-geodatabase-shapefiles/
+    bash = 'ogr2ogr -f "PostgreSQL" PG:"dbname={dbname} user={user}" "{geojson}" -nln {output_table_name} -overwrite'.format(
+            dbname = dbname,
+            user = username,
+            geojson = geojson,
+            output_table_name = output_table_name)
+    system(bash)
+    
+def createPostgreSQLTable(db_name, user, password, table_name, overwrite = False):
+    try:
+        con = psycopg2.connect("dbname={} user={} password={}".format(db_name, user, password))
+        cursor = con.cursor()
+        print "connected"
+    except:
+        print "connection failed!"
+        
+    if overwrite == True:
+        del_table_query = """DROP TABLE IF EXISTS {table_name};""".format(table_name = table_name)
+        cursor.execute(del_table_query)
+    insert_query = """CREATE TABLE IF NOT EXISTS {table_name} (
+                    id  	bigint,
+                    time	varchar(50),
+                    latitude	decimal,
+                    longitude	decimal,
+                    selfrepcity varchar(500),    
+                    lang	varchar(10),
+                    source	varchar(250),
+                    countrycode	varchar(10),
+                    countryname	varchar(250),
+                    location	varchar(250),
+                    url	varchar(100),
+                    text        varchar(500),
+                    loclat   decimal,
+                    loclong  decimal);
+                """.format(table_name = table_name)
+    cursor.execute(insert_query)
+    con.commit()
+    cursor.close()
+    con.close()
+
+def exportPostgresqltoGeojson(dbname, username, password, output_filename):
+    """Using bash, export a postgresql table to GeoJSON format"""
+    # Export temporary table to GeoJSON
+    bash = 'ogr2ogr -f "GeoJSON" {geojson}.geojson PG:"dbname={dbname} user={user} password={password}" "temp"'.format(
+        dbname = dbname,
+        user = username,
+        password = password,
+        geojson = output_filename)    
+    system(bash)  
+
+def filterTweetsToData(all_tweets):
+    """Select relevant data from all returned records for marker assignment"""
+    # TODO: Use pandas Dataframe as data sharing mechanism for direct column name assignment
+    list_of_tweets = []
+    for i, record in enumerate(all_tweets):
+        tweet_text = record[11].encode('utf-8')
+        tweet_sentiment_overall = record[14]
+        tweet_label = record[15]
+        
+        
+        # If it can use accurate coordinates, add these coordinates and True, else the estimated coords with False
+        if record[2] != 9999:
+            tweet_coords = (float(record[2]),float(record[3]), True)
+        else:
+            tweet_coords = (float(record[12]),float(record[13]), False)
+        
+        list_of_tweets.append([tweet_text, (tweet_sentiment_overall, tweet_label), tweet_coords])
+    return list_of_tweets
--- Final_assignment/main.py
+++ Final_assignment/main.py
@@ -6,26 +6,31 @@ Created on Wed Feb  1 10:51:48 2017
 names: Alex Levering and Hector Muro
 """
 
-import createTable
-import gatherData
-import convertFormats
-import sentimentAnalyzerVader #MUST FIX ISSUES HERE
-import storeSentimentData
+# Change working directory to load scripts if needed
+import os
+os.chdir(r"/home/user/git/SatoshiNakamotoGeoscripting/Final_assignment")
 
+from lib import gatherData
+from lib import dataManagement
+from lib import sentimentAnalyzerVader #MUST FIX ISSUES HERE
+from lib import storeSentimentData
+from lib import visualizeData
+
+os.chdir(r"/home/user/git/SatoshiNakamotoGeoscripting/Final_assignment/data")
 
 """Database must be created beforehand manually in Postgres"""
 #createTable.createTable(db_name=, user=, password=, table_name=, overwrite = False)
-createTable.createTable(db_name="tweets", user="user", password="user", table_name = "trumptweets", overwrite = True)
+dataManagement.createPostgreSQLTable(db_name="tweets", user="user", password="user", table_name = "trumptweets", overwrite = False)
 
 """Connection with Twitter in real-time gathering data. The table where tweets are going to be inserted is set by default "trumptweets"
     If another name is wanted, or the name is changed in the create Table call, it must be changed manually inside gatherData.py"""
-gatherData.TweetsRealTime()
+# gatherData.TweetsRealTime()
 
 """Retrieve tweets for further sentiment analysis, selecting only those in english because the sentiment library understands English only"""
 sql = "SELECT * FROM trumptweets WHERE lang = 'en' or lower(lang) = 'en-GB' or lower(lang) = 'en-US'"
-tweets = convertFormats.getTweetsFromDB("tweets","user", "user", sql)
+tweets = dataManagement.getTweetsFromDB("tweets","user", "user", sql)
 
-"""Sentimet analysis"""
+"""Sentiment analysis"""
 """This requires the manual download of vader_lexicon.txt. Use nltk.download() once nltk library is downloaded"""
 #import nltk
 #nltk.download
@@ -38,8 +43,34 @@ storeSentimentData.updateColumns(db_name="tweets", user="user", password="user",
                                  sentiment_table="sentiment_table", list_columns = ["label","sentiment"], list_type=["varchar(15)", "numeric"])
 
 
+"""Visualize data"""
+tweets = dataManagement.getTweetsFromDB(dbname = "tweets",
+                                         username = "user",
+                                         password = "user",
+                                         sql = "SELECT * FROM trumptweets")[-10:]
+                         
+dataManagement.importPolyJSON(dbname = "tweets",
+                               username = "user",
+                               password = "user",
+                               geojson = "countries.geo.json",
+                               output_table_name = "countrydata")
+                              
+dataManagement.exportPostgresqltoGeojson(dbname = "tweets",
+                                          username = "user",
+                                          password = "user",
+                                          output_filename = "tweetspercountry")                              
+              
+records = visualizeData.getPointsPerPolygon(dbname = "tweets",
+                                          username = "user",
+                                          password = "user",
+                                          poly_table_name = "countrydata",
+                                          tweet_table_name = "trumptweets")
 
-
-#if __name__ == '__main__':
-#
-#    
+filtered_tweets = dataManagement.filterTweetsToData(tweets)
+              
+trump_tweets = visualizeData.tweetMap(filtered_tweets, 3, "cartodbpositron")
+trump_tweets.addTweets()
+trump_tweets.addChoropleths("tweetspercountry.geojson", records)
+trump_tweets.addPolygonCentroids("tweetspercountry.geojson", records)
+trump_tweets.addLayerControl()
+trump_tweets.saveMap("Trump Tweets Test")
