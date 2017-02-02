c891e8e53abb44708eda251fcdded3c2bad32d19
user <muro.hector@gmail.com>
2017-02-02 14:54:15 +0100

    Deleted secret keys

--- Final_assignment/main.py
+++ Final_assignment/main.py
@@ -15,8 +15,6 @@ Non-standard dependencies:
 TO DO BEFOREHAND:
 The following steps are non-automatable and have to be performed manually.
 * Have the NLTK vader lexicon locally (nltk.download("vader"))
-   - It may be that the lexicon does not install in the right location
-     If this is the case, locate the file and place it #TODO: FILEPATH
 * Have PostGIS installed on PostgreSQL
 * Set the file paths specified below to wherever your folder is
 * Upgrade folium to the latest version (0.2.1)
@@ -26,8 +24,10 @@ The following steps are non-automatable and have to be performed manually.
 
 # Naming options for tables, intermediates and outputs are available in the wrapper.
 if __name__ == "__main__":
+    """The tool is not supplied with Tweets out-of-the-box. Set 'gather_data' to True and leave it
+        running for a while. If loop is false it will terminate in a minute or so and create a map from the results automatically"""
     import tweetAnalysisWrapper
-    tweetAnalysisWrapper.performTweetResearch(folder_path = r"/home/user/git/SatoshiNakamotoGeoscripting/Final_assignment",
+    tweetAnalysisWrapper.performTweetResearch(folder_path = r"/home/user/Desktop/GIT/Final_assignment",
                                               defaultdb = "postgres",
                                               user = "user",
                                               password = "user",
@@ -35,7 +35,7 @@ if __name__ == "__main__":
                                               tweet_table_name = "tweets",
                                               gather_data = True,
                                               loop_gathering = False,
-                                              APP_KEY = "Q530eYJ2divtkAltNRF9ORY6G",
-                                              APP_SECRET =  "xgRzCBf53goOm1ir06spIT8oAmvQFu5kr53ptycDn4CCEw0MYc",
-                                              OAUTH_TOKEN =  "2567730218-I3fdSSmhVi8vDq0zn94OGTnfkpTpPqKpOHaqvD5",
-                                              OAUTH_TOKEN_SECRET = "maoKC8LRS2rxpRmSz9mUbOCTc8TE2VxAaBJQTue2stqQS")
+                                              APP_KEY = "",
+                                              APP_SECRET =  "",
+                                              OAUTH_TOKEN =  "",
+                                              OAUTH_TOKEN_SECRET = "")
